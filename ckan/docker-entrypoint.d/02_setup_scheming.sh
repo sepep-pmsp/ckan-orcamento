c28fb5ebdc17a6631ad379f29e6d561e6f778e36
folhesgabriel <pisagabriel09@gmail.com>
2025-05-21 07:47:20 -0300

    remove ckanext-scheming setup

--- ckan/docker-entrypoint.d/02_setup_scheming.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ckan config-tool $CKAN_INI "scheming.dataset_schemas = ckanext.scheming:ckan_dataset_schema.yaml"
-ckan config-tool $CKAN_INI "scheming.presets = ckanext.scheming:presets.json"

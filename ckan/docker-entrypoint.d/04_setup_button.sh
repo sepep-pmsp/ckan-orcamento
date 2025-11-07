251e83bc15eb6efc304e8538c0bbcfc83a27b98e
folhesgabriel <pisagabriel09@gmail.com>
2025-11-07 15:53:09 +0000

    fix: treafik proxy timeout labels

---
+++ ckan/docker-entrypoint.d/04_setup_button.sh
@@ -0,0 +1,8 @@
+echo "Forcing ckan.uploads_enabled = true"
+ckan config-tool $CKAN_INI "ckan.uploads_enabled = true"
+
+echo "Forcing ckan.storage_path = /var/lib/ckan"
+ckan config-tool $CKAN_INI "ckan.storage_path = /var/lib/ckan"
+
+echo "Forcing ckan.requests.timeout = 300"
+ckan config-tool $CKAN_INI "ckan.requests.timeout = 300"

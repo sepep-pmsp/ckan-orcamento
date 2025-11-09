f5d8f379834f450ac748f3096ffbea1bdf79036b
folhesgabriel <pisagabriel09@gmail.com>
2025-11-09 19:47:40 +0000

    feat: add dagster UI

--- ckan/docker-entrypoint.d/04_setup_button.sh
+++ ckan/docker-entrypoint.d/04_setup_button.sh
@@ -6,3 +6,6 @@ ckan config-tool $CKAN_INI "ckan.storage_path = /var/lib/ckan"
 
 echo "Forcing ckan.requests.timeout = 300"
 ckan config-tool $CKAN_INI "ckan.requests.timeout = 300"
+
+echo "Forcing ckan.requests.timeout = 300"
+ckan config-tool $CKAN_INI "ckan.locale_default = pt_BR"
